8b927f9c7072110217c5a4c74f95f011b532c3d3
Kiritoo06 <112476702+Kiritoo06@users.noreply.github.com>
2022-11-14 08:39:26 -0800

    first

---
+++ Index.js
@@ -0,0 +1,88 @@
+// Taski 1
+// let a=prompt("Mon")
+// let b="";
+// for (let i=0; i<a.length; i++){
+//     b=b+a[i]+a[i]
+// }
+// console.log(b)
+
+// Taski 2
+// var cnt=0;
+// let a=prompt("Mon")
+// let b=a.split("potato")
+// for(i=0;i<b.length;i++){
+//     cnt++;
+// }
+// console.log(cnt-1)
+
+// Taski 3
+// let a=prompt(" ")
+// let b=prompt("")
+// var cnt=0;
+// for(i=0;i<a.length;i++){
+//     if(a[i]==b){
+// cnt++;
+//     }
+// }
+// console.log(cnt)
+
+// Taski 4
+// How many vowles
+// var cnt=0;
+// let a=prompt("Mon")
+// let b=a.split("")
+// for(i=0;i<b.length;i++){
+//     if(b[i]=="a" || b[i]=="e" || b[i]=="i" || b[i]=="o" || b[i]=="u"){
+//         cnt++;
+//     }
+// }
+// console.log(cnt)
+
+// Taski 5
+// let a=prompt("Mon")
+// console.log(a.includes("bomb")?"Duck !!!":"There is not bomb relax")
+
+// Taski 6
+// let a=prompt("Mon")
+// let b=a.replace(/a/gi,"")
+// let c=b.replace(/e/gi,"")
+// let d=c.replace(/i/gi,"")
+// let f=d.replace(/o/gi,"")
+// let g=f.replace(/u/gi,"")
+// console.log(g)
+
+
+// // Taski 7
+// let a=prompt("Mon")
+// let b=a.split("")
+// for(i=0;i<a.length;i++){
+//     if(b[i]=="a" || b[i]=="o" || b[i]=="u" || b[i]=="i" || b[i]=="y"){
+//         console.log(i)
+//         break;
+//     }
+// }
+
+//  Taski 8 {Uvereni 99% / 1% ne uvereni}
+// let a=prompt("Mon")
+// let b=+prompt("Mon1")
+// console.log(b==1?`Hello ${a.slice(0,1).toUpperCase()+a.slice(1).toLowerCase()}`: `Bye ${a}`)
+
+//  Taski 9 {Nashid}
+// let a=prompt("Mon")
+// let b=+prompt("Mon1")
+// console.log(`${(b*100)/148.9e+6} is ${a} of the total world's landmass`)
+
+// Taski 10
+// let a=prompt("Rare ")
+// let b=a.split(" ")
+// let c=b.reverse()
+// let d=c.join(" ")
+// console.log(d)
+
+// Taski 11
+// let a=prompt()
+// let b=a.split("")
+// let c=b.reverse()
+// console.log(c.join("")+a)
+
+
